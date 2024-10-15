a0146e41e979382217526ead11630efe4bc2a26f
michaelmugeni <michaelmugeni98@gmail.com>
2024-10-15 01:39:43 +0000

    3-tasks.py

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    return asyncio.create_task(wait_random(max_delay))
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def test(max_delay: int) -> float:
+    task = task_wait_random(max_delay)
+    await task
+    print(task.__class__)
+
+asyncio.run(test(5))
+
