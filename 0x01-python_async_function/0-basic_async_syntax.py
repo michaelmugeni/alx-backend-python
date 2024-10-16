9a87ab86cb4821a58ad773e582e914ad0bc62b8b
michaelmugeni <michaelmugeni98@gmail.com>
2024-10-16 11:32:14 +0000

    added updated 0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -13,3 +13,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 print(asyncio.run(wait_random(10))) #9.034261504534394
 print(asyncio.run(wait_random(2))) #1.6216525464615306
 print(asyncio.run(wait_random(15))) #10.634589756751769
+
