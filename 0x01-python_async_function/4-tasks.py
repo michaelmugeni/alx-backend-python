c2e053c5c6d3a68d082283438a6d8283f1c8f0d6
Michael <136024661+michaelmugeni@users.noreply.github.com>
2024-10-17 02:12:27 +0300

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,41 +1,19 @@
 #!/usr/bin/env python3
-"""Execute multiple coroutines at the same time with async
-mandatory
+"""
+A module for creating and running multiple asyncio Tasks concurrently.
 """
 import asyncio
 from typing import List
-
-
-wait_random = __import__('0-basic_async_syntax').wait_random
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn task_wait_random function n times
-
-    Args:
-        n (int): number of time wait _random should be callled.
-        max_delay (int): delay period
-
-    Returns:
-        List[float]: List of all the delays in sorted order
     """
-    # gather with an unpacked list of awaitables
-    res = await asyncio.gather(*(task_wait_random(max_delay)
-                               for _ in range(n)))
-
-    # Sort result in ascending order
-    for i in range(len(res)):
-        for j in range(i+1, len(res)):
-            if (res[i] > res[j]):
-                res[i], res[j] = res[j], res[i]
-
-    return res
-
-
-task_wait_n = __import__('4-tasks').task_wait_n
-
-n = 5
-max_delay = 6
-print(asyncio.run(task_wait_n(n, max_delay)))
-
+    Create and run `n` asyncio Tasks that wait for random amounts of time
+    up to `max_delay` seconds. Returns a list of the Task objects, sorted
+    in ascending order by the time waited.
+    """
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
