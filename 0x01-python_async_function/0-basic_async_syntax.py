fcceeed43ff20a0f0540cb4adf4f07e5c8be36d3
michaelmugeni <michaelmugeni98@gmail.com>
2024-10-16 11:26:39 +0000

    newly updated 0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -10,11 +10,6 @@ async def wait_random(max_delay: int) -> float:
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-print(asyncio.run(wait_random(10 ))) # 9.034261504534394
-print(asyncio.run(wait_random(2)))  # 1.6216525464615306
-print(asyncio.run(wait_random(15))) # 10.634589756751769
-
-# To run th main function
-if __name__ == "__main__":
-    asyncio.run(main())
-
+print(asyncio.run(wait_random(10))) #9.034261504534394
+print(asyncio.run(wait_random(2))) #1.6216525464615306
+print(asyncio.run(wait_random(15))) #10.634589756751769
